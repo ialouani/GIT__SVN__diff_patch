689db612e473a6fb8d38e0b22b40c7edb68ed97c
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-04-28 11:52:28 +0000

    release number 4 for 2.3/3.3

---
+++ build-statelib-Desktop-Debug2/CMakeFiles/state.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/state.dir/src/state.c.o"
+  "libstate.pdb"
+  "libstate.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/state.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
